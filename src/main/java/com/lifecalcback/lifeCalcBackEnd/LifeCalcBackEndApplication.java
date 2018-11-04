37829d8fb640f3f75aace9e9d76fd4e1ddf36f48
V1centR <vicentcdb@gmail.com>
2018-11-03 23:55:03 -0300

    setup Project springBoot

---
+++ src/main/java/com/lifecalcback/lifeCalcBackEnd/LifeCalcBackEndApplication.java
@@ -0,0 +1,12 @@
+package com.lifecalcback.lifeCalcBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeCalcBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeCalcBackEndApplication.class, args);
+	}
+}
